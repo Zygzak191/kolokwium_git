229966e54ca34574de773a21600fcbf98139cd6e
Zygzak191 <karol.bialas@smcebi.edu.pl>
2020-01-17 09:14:04 +0100

    Kolokwium-Karol Białas

---
+++ Zadanie5/build/CMakeFiles/myProgram.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/karol/kolokwium_git/Zadanie5/zrodlo/address.cpp" "/home/karol/kolokwium_git/Zadanie5/build/CMakeFiles/myProgram.dir/address.cpp.o"
+  "/home/karol/kolokwium_git/Zadanie5/zrodlo/employee.cpp" "/home/karol/kolokwium_git/Zadanie5/build/CMakeFiles/myProgram.dir/employee.cpp.o"
+  "/home/karol/kolokwium_git/Zadanie5/zrodlo/main.cpp" "/home/karol/kolokwium_git/Zadanie5/build/CMakeFiles/myProgram.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/karol/kolokwium_git/Zadanie5/zrodlo"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
